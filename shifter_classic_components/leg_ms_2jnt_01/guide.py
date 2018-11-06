fade4d05caba9084fd90f987812230f376143e69
Jerome Drese <jdrese@gmail.com>
2018-11-06 12:15:25 +0100

    Fix fcurve division query issue closes #19

--- shifter_classic_components/leg_ms_2jnt_01/guide.py
+++ shifter_classic_components/leg_ms_2jnt_01/guide.py
@@ -83,6 +83,11 @@ class Guide(guide.ComponentGuide):
         self.pParentJointIndex = self.addParam(
             "parentJointIndex", "long", -1, None, None)
 
+    def get_divisions(self):
+        """ Returns correct segments divisions """
+
+        self.divisions = self.root.div0.get() + self.root.div1.get() + 5
+        return self.divisions
 
 ##########################################################
 # Setting Page
